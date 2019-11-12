b510ebe9edbbec8b199d703186d7cb689021077e
Nigel Alford <nigel@repairhero.tech>
2019-11-12 09:43:18 -0500

    drum machine works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild, ElementRef } from "@angular/core";
+import { pad } from './beats.model';
 
 @Component({
   selector: "app-root",
@@ -6,81 +7,185 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  BPM = 120;
-  TICKS = 16;
-  INTERVAL = 1 / ((4 * this.BPM) / (60 * 1000));
-  MAX_BITS = 32;
-  MAX_HEX = this.MAX_BITS / 4;
+  @ViewChild('drumPad', {static: false})
+  drumPad: ElementRef;
+
   bars = Array(16)
     .fill()
     .map((x, i) => i);
+  beat
 
-  Sounds = [
+  Instruments = [
     {
       title: "bass_drum",
-      location: "assets/bass_drum.wav"
+      location: "./assets/sounds/bass_drum.wav",
+      active: false
     },
     {
       title: "cl_hi_hat",
-      location: "assets/cl_hi_hat.wav"
+      location: "./assets/sounds/cl_hi_hat.wav",
+      active: false
     },
     {
       title: "claves",
-      location: "assets/claves.wav"
+      location: "./assets/sounds/claves.wav",
+      active: false
     },
     {
       title: "cowbell",
-      location: "assets/cowbell.wav"
+      location: "./assets/sounds/cowbell.wav",
+      active: false
     },
     {
       title: "cymbal",
-      location: "assets/cymbal.wav"
+      location: "./assets/sounds/cymbal.wav",
+      active: false
     },
     {
       title: "hand_clap",
-      location: "assets/hand_clap.wav"
+      location: "./assets/sounds/hand_clap.wav",
+      active: false
     },
     {
       title: "hi_conga",
-      location: "assets/hi_conga.wav"
+      location: "./assets/sounds/hi_conga.wav",
+      active: false
     },
     {
       title: "hi_tom",
-      location: "assets/hi_tom.wav"
+      location: "./assets/sounds/hi_tom.wav",
+      active: false
     },
     {
       title: "low_conga",
-      location: "assets/low_conga.wav"
+      location: "./assets/sounds/low_conga.wav",
+      active: false
     },
     {
       title: "low_tom",
-      location: "assets/low_tom.wav"
+      location: "./assets/sounds/low_tom.wav",
+      active: false
     },
     {
       title: "maracas",
-      location: "assets/maracas.wav"
+      location: "./assets/sounds/maracas.wav",
+      active: false
     },
     {
       title: "mid_conga",
-      location: "assets/mid_conga.wav"
+      location: "./assets/sounds/mid_conga.wav",
+      active: false
     },
     {
       title: "mid_tom",
-      location: "assets/mid_tom.wav"
+      location: "./assets/sounds/mid_tom.wav",
+      active: false
     },
     {
       title: "o_hi_hat",
-      location: "assets/o_hi_hat.wav"
+      location: "./assets/sounds/o_hi_hat.wav",
+      active: false
     },
     {
       title: "rim_shot",
-      location: "assets/rim_shot.wav"
+      location: "./assets/sounds/rim_shot.wav",
+      active: false
     },
     {
       title: "snare_drum",
-      location: "assets/snare_drum.wav"
+      location: "./assets/sounds/snare_drum.wav",
+      active: false
     }
   ];
 
-  title = "drummerboy";
+  padMap: pad[] = new Array;
+
+  title = "little drummerboy";
+
+  _sampleRate = 44100;
+  _minuteInSeconds = 60;
+  _beatsPerMinute = 140;
+  _totalSteps = 16;
+
+  _stepDelay;
+
+  _isPlaying = false;
+  _currentBar = 0
+
+  calculateBPM = () => {
+    this._stepDelay = Math.round(
+      (this._sampleRate * this._minuteInSeconds) /
+        (this._beatsPerMinute * this._totalSteps) /
+        this._totalSteps
+    );
+
+    return this._stepDelay;
+  };
+
+  playPause = () => {
+    this.calculateBPM();
+
+    if (this._isPlaying) {
+      this.stopAudio();
+      this._isPlaying = false;
+    } else {
+      this._isPlaying = true;
+      this.beat = setInterval(() => {
+        this.startAudio();
+      }, this._stepDelay)
+    }
+  };
+
+  updateBPM = (bpm) => {
+    bpm = parseInt(bpm, 10)
+    this._beatsPerMinute = bpm;
+
+    this.calculateBPM();
+
+    this.playPause();
+    this.playPause();
+  };
+
+  stopAudio = () => {
+    clearInterval(this.beat)
+  };
+
+  playAudio = (location) => {
+    const audio = new Audio;
+    audio.src = location;
+    audio.load();
+    audio.play();
+
+    setTimeout(() => {
+      audio.pause();
+    },1000)
+
+    this._isPlaying = true
+  }
+
+  startAudio = () => {
+    if(this._currentBar < this.bars.length) {
+      this._currentBar++;
+    }else if(this._currentBar >= this.bars.length){
+      this._currentBar = 1;
+    }
+
+    this.padMap.forEach (pad => {
+      if(pad.position+1 === this._currentBar && pad.active) {
+        this.playAudio(pad.location);
+      }
+    })
+  };
+
+  toggleStatus = (btn: pad, padState) => {
+    const pad = this.padMap.filter(pad => (pad._id === btn._id))
+
+    if(pad.length) {
+      pad[0].active = !pad[0].active
+    } else {
+      this.padMap.push(btn)
+    }
+
+    padState = !padState
+  }
 }
---
+++ src/app/beats.model.ts
@@ -0,0 +1,7 @@
+export class pad {
+  title: String;
+  location: String;
+  active: Boolean;
+  position?: number;
+  _id:? number;
+}
